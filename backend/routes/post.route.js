d1ec1aa799540050b1d34bc16ca82779c1683c96
youngman20 <chhayankpatel209@gmail.com>
2025-02-02 16:41:19 +0530

    configured render

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -15,9 +15,10 @@ const router = express.Router();
 router.get("/", protectRoute, getFeedPosts);
 router.post("/create", protectRoute, createPost);
 router.delete("/delete/:id", protectRoute, deletePost);
-router.get("get/:id", protectRoute, getPostById);
-router.post("post/:id/comment", protectRoute, createComment);
-router.post("post/:id/like", protectRoute, likePost);
+router.get("/get/:id", protectRoute, getPostById);
+router.post("/post/:id/comment", protectRoute, createComment);
+router.post("/post/:id/like", protectRoute, likePost);
 router.get("/user-posts", protectRoute, userPosts);
 
 export default router;
+
